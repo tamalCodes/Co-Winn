eb4f28798682512f20cc4a0d219823aa34d4986d
Tamal Das <gyansujan69@gmail.com>
2021-05-23 09:55:32 +0530

    This a basic covid 19 website

---
+++ style.js
@@ -0,0 +1,42 @@
+
+
+
+tippy("#doc", {
+  content: "Consult a doctor",
+  followCursor: 'horizontal',
+  theme: 'tomato',
+});
+
+
+tippy("#food", {
+    content: "Get food delivered",
+    followCursor: 'horizontal',
+    theme: 'tomato',
+  });
+
+  
+  tippy("#beds", {
+    content: "Find beds nearby",
+    followCursor: 'horizontal',
+    theme: 'tomato',
+  });
+
+
+  tippy("#o2", {
+    content: "Get an oxygen tank",
+    followCursor: 'horizontal',
+    theme: 'tomato',
+  });
+
+   document.getElementById("lg").addEventListener("click", function () {
+     document.getElementById("loginform").style.display = "block";
+   });
+  
+  document.querySelector(".close").addEventListener("click", function () {
+     document.getElementById("loginform").style.display = "none";
+  });
+  
+
+  document.getElementById("sbmtbtn").addEventListener("click", function () {
+    document.getElementById("loginform").style.display = "none";
+  });
